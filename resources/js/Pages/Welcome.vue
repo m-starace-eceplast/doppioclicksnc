95e3f4029b64e5b4f90302ced32b8c9e65e2f399
Marco Giuseppe <m.starace@eceplast.it>
2025-10-04 13:08:04 +0200

    dep(): add dependencies vue vuetify inertia and create or modify another file

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+const handleClick = () => {
+    alert('Stack configurato correttamente!')
+}
+</script>
+
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row justify="center">
+                    <v-col cols="12" md="8">
+                        <v-card>
+                            <v-card-title>
+                                <h1 class="text-h4">Benvenuto in Laravel + Vue + Vuetify + Inertia!</h1>
+                            </v-card-title>
+                            <v-card-text>
+                                <p>Il tuo stack è configurato correttamente.</p>
+                                <v-btn color="primary" @click="handleClick">
+                                    <v-icon left>mdi-heart</v-icon>
+                                    Clicca qui
+                                </v-btn>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,34 @@
 import './bootstrap';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'light'
+    }
+})
+
+createInertiaApp({
+    title: (title) => `${title} - ${import.meta.env.VITE_APP_NAME}`,
+    resolve: (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(vuetify)
+            .mount(el)
+    },
+    progress: {
+        color: '#4B5563',
+    },
+})
