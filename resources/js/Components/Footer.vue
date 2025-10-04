bb2a0aec827362b06da9ab4fb04567f8378b7fdd
Marco Giuseppe <m.starace@eceplast.it>
2025-10-05 01:00:00 +0200

    feat(footer): add Footer component with copyright information

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <v-footer
+  class="d-flex align-center justify-center text-center"
+  height="40"
+>
+  <span class="text-subtitle-1 font-weight-medium">
+    © {{ new Date().getFullYear() }} Doppio Click SNC — Tutti i diritti riservati
+  </span>
+</v-footer>
+</template>
